760744f73f2aa57bb2b9cdb28ce2e3280ae049da
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2020-05-29 18:52:31 +0300

    Update readfle.c

--- Ergasia_1/readfle.c
+++ Ergasia_1/readfle.c
@@ -1,3 +1,4 @@
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,19 +6,44 @@
 #include "mmio.c"
 #include "mmio.h"
 
+#define T_NUM 2
+
 typedef struct {
   int num;
   int va8mos;
-  int s; //size of geitones (int)
-  int b_size;//se bytes,gia geitones
-  int *geitones;
+  int s;  //  size of geitones (int)
+  int b_size;  //  se bytes,gia geitones
+  int *geitones;  //  ari8mos geitona
+  omp_lock_t nlock;
 }node;
 
 typedef struct{
-  int b_size;
-  int num;
-  node *stk;
+  int b_size;  //  mege8os stoivas se byte
+  int num;  //  mege8os stoivas (int)
+  node *stk;  //  h stoiva
+  omp_lock_t nlock;
 }m_stack;
+
+/*
+ sunarthsh gia na
+ kratw mono tous geitones,
+ pou me endiaferoun
+*/
+void limit_neighbours(node *komvos,m_stack *stk){
+    //arxikopoihsh komvou
+    free(komvos->geitones);
+    komvos->geitones = (int *)malloc(sizeof(int));
+    komvos->b_size = 0;
+    //orismos twn geitonwn me va8mo eisodou = 0
+    for(int i = 0; i<stk->num; i++){
+      komvos->b_size += sizeof(stk->stk[i].num);
+      komvos->geitones = (int *)realloc(komvos->geitones,sizeof(komvos->b_size));
+      komvos->geitones[i] = stk->stk[i].num;
+    }
+    komvos->s = stk->num;
+    komvos->geitones = (int *)realloc(komvos->geitones,komvos->b_size + sizeof(int) );
+    komvos->geitones[komvos->s] = -1;//to telos twn geitwnwn
+}
 
 /*
 pairnei enan komvo kai vgazei ton prwto se seira geitona
@@ -46,6 +72,13 @@ void stk_push(m_stack *stk,node *n){
 }
 
 node stk_pop(m_stack *stk){
+  //vlepw an h stoiva einai kenh
+  if(stk->num==0){
+    node n;
+    n.num = -1;
+    return n;
+  }
+  //pop an element
   stk->num-=1;
   stk->b_size-= sizeof(stk->stk[stk->num]);
   node ret = stk->stk[stk->num];
@@ -53,7 +86,15 @@ node stk_pop(m_stack *stk){
   return ret;
 }
 
+/*
+ pairnei ena sunolo komvwn kai topo8etei tous komvous me va8mo eisodou 0 se mia stoivas
+          **** orismata ****
+           arr -> sunolo komvwn
+           num -> mege8os arr se int
+           stk -> h stoiva
+*/
 void stk_init(node *arr,int num,m_stack *stk){
+  //stk->on_process = 0;
   stk->num = 0;
   stk->b_size = 1;
   stk->stk = (node *)malloc(sizeof(node *));
@@ -70,6 +111,7 @@ void stk_init(node *arr,int num,m_stack *stk){
 
   }
 }
+
 
 void cook(node *n1,node *n2,int geit){
   //n --> node
@@ -91,45 +133,37 @@ void cook(node *n1,node *n2,int geit){
   n2->s+=1;
 }
 
+/*
+o algori8mos tou kahn
+orismata :
+        graph ->  oloklhro to grafhma
+        sorted -> komvos me va8mo eisodou == 0
+*/
+m_stack topological_sort(node *graph,node *sorted){
 
-unsigned int topological_sort(node *graph, unsigned int size,node *sorted){
+  printf("Eksetazetai %i\n", sorted->num);
+  m_stack stack;
+  //arxikopoihsh ths stoivas
+  stack.num = 0;
+  stack.b_size=0;
+  stack.stk = (node *)malloc(sizeof(node *));
 
-    //allagh
-    unsigned int sorted_size = 0;
-    node first ;
-
-    m_stack stack;
-    stack.num = 0;
-    stk_init(graph,size,&stack);
-
-    // Get the nodes with no incoming edges
-    //allagh
-    unsigned int nodes_size = stack.num ; //To mege8os ths stoivas
-    // Main loop
-    if(nodes_size == 0){return 1;}
-    while (nodes_size > 0) {
-        // Get first node
-        first = stk_pop(&stack);
-        printf("Eksetazetai %i\n", first.num);
-        //vlepw poioi komvoi exoun va8mo eisodou 0
-        for(int i = 0; i<first.s; i++){
-
-          graph[ first.geitones[i] ].va8mos -= 1;
-          if(graph[ first.geitones[i] ].va8mos == 0){
-            node temp = *(graph + first.geitones[i] );
-            stk_push(&stack ,&temp );
-          }
-
-        }
-        // Add it to the sorted array
-        sorted[sorted_size] = first;
-        sorted_size++;
-
-        //ananewnw to mege8os ths stoivas
-        nodes_size = stack.num ;
+  if(sorted->num == -1){return stack;}
+  // Main loop
+  for(int i = sorted->s; i>0; i--){
+    //vlepw poioi komvoi exoun va8mo eisodou 0
+    omp_set_lock( &graph[ sorted->geitones[sorted->s-i] ].nlock );
+    graph[ sorted->geitones[sorted->s-i] ].va8mos -= 1;
+    omp_unset_lock( &graph[ sorted->geitones[sorted->s-i] ].nlock );
+    //sto shmeio auto uparxei mono 1 thread to
+    //opoio vlepei pws o va8mos eisodou einai isos me 0
+    if(graph[ sorted->geitones[sorted->s-i] ].va8mos == 0){
+      node temp = *(graph + sorted->geitones[sorted->s-i] );
+      stk_push(&stack ,&temp );
     }
+  }
 
-    return 0;
+    return stack;
 }
 
 
@@ -138,13 +172,13 @@ void main(){
   //posoi komvoi
   int m_size;
   int line[2];
-
+  //sxetika me mmio
   MM_typecode t;
-
-  m_stack stack;
+  //h stoiva ths main
+  m_stack main_stack;
 
   FILE *f;
-  f = fopen("C:\\Users\\aleda\\Downloads\\ParallhlhEpeksergasia-master\\ParallhlhEpeksergasia-master\\Ergasia_1\\ibm32.mtx","r+");
+  f = fopen("/home/g4d/Desktop/eeee/ibm32.mtx","r+");
 
   if (mm_read_banner(f,&t) != 0)
   {
@@ -155,17 +189,19 @@ void main(){
   //returns 0 in case of error
   mm_read_mtx_array_size(f,&m_size,&m_size);
   printf("%i\n", m_size);
+  //apo8hkeush olwn twn komvwn se enan pinaka
   node arr[m_size];
-
-
+  //uparxei ena lock gia ka8e komvo tou grafhmatos
+  //init locks
   //Arxikopoihsh
   for(int j=0; j<m_size; j++){
+
     arr[j].num = j+1;
     arr[j].va8mos = 0;
     arr[j].s = 0;
     arr[j].b_size = 0;
+    omp_init_lock(&arr[j].nlock);
   }
-
   //Diavasma Arxeiou
   while(!feof(f))
    {
@@ -176,19 +212,72 @@ void main(){
    }
    fclose(f);
 
-  //gia test sto function pop_a_neighbor()
-  //printf("%d\n", pop_a_neighbor(&arr[0]) );
-
-  const unsigned int order = m_size; // nodes
-  node acyclic[order];
-
-  if(topological_sort(arr, order, acyclic)){
-    printf("To Grafhma Einai Kukliko\n");
+  //gemizw thn main_stack
+  stk_init(arr,m_size,&main_stack);
+  if(main_stack.num==0){
+    printf("To grafhma einai kukliko\n");
     exit(0);
   }
-  for (int i = 0; i < order; i++) {
-      printf("%u \n", acyclic[i].num);
-  }
 
-  printf("Telos\n");
+  //o komvos pou eksetazetai
+  node on_process;
+
+  //posa threads
+  omp_set_num_threads(T_NUM);
+
+  /*
+  uparxei mia metavlhth shared koinh,
+  gia ola ta threads shmatodotei
+  to pote teleiwnei
+  */
+  int shared = m_size;
+  omp_lock_t shared_lock;
+  omp_init_lock(&shared_lock);
+
+  #pragma omp parallel
+  {
+    while(shared>0){
+      omp_set_lock(&main_stack.nlock);
+      on_process = stk_pop(&main_stack);
+      omp_unset_lock(&main_stack.nlock);
+
+
+    //ean h stoiva einai kenh to thread perimenei
+    while(1){
+      #pragma omp critical
+      {
+        if(on_process.num==-1){
+          //h stoiva einai kenh
+          on_process = stk_pop(&main_stack);
+        }
+      }
+      if( !(on_process.num==-1) ){break;}
+    }
+    //sto shmeio auto ka8e thread exei enan komvo me va8mo eisodou == 0
+    m_stack temp_stack = topological_sort(arr,&on_process);
+    /*
+
+    h metavlhth temp stack
+    periexei tous geitones tou komvou
+    pou eksetasthke me va8mo eisodou == 0
+
+    */
+
+    //ston pinaka oi geitones tou komvou meiwnontai se aytous pou exoun va8mo eisodou == 0
+    limit_neighbours(&arr[on_process.num],&temp_stack);
+    omp_set_lock(&main_stack.nlock);
+    for(int j=0; j<=temp_stack.num; j++){
+      node n_temp = stk_pop(&temp_stack);
+      stk_push(&main_stack,&n_temp );
+    }
+    omp_unset_lock(&main_stack.nlock);
+
+    omp_set_lock(&shared_lock);
+    shared-=1;
+    omp_unset_lock(&shared_lock);
+    //end of while loop
+  //end of omp parallel
+  }
+ }
 }
+//end of main
